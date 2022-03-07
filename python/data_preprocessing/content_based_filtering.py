a279ca5006c9098d818ffcabf5a2351a8aaf554e
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-07 16:47:17 +0900

    2022.03.07 컨텐츠 기반 필터링

--- python/data_preprocessing/content_based_filtering.py
+++ python/data_preprocessing/content_based_filtering.py
@@ -1,50 +1,40 @@
-from ast import literal_eval
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+import time
+
+start = time.time() # 시간 측정
 
 # 향수 데이터 df 생성 및 사용할 칼럼 추출
-df = pd.read_json('perfumes.json')
-data = df[['main_accords', 'notes', 'perfume']].copy()
+df = pd.read_json('data_preprocessing.json')
 
+# data = df[['main_accords', 'notes', 'perfume']].copy()
+# print('data 복사 시간 : ', time.time() - start)
 
-# 문자열로 된 list, dict --> 실제 list, dict 으로 변환
-for i in range(len(data['notes'])):
-    data['notes'][i] = data['notes'][i].replace('null', 'None')
-    data['main_accords'][i] = data['main_accords'][i].replace('null', 'None')
+sum_data = df['main_accords'] + ' ' + df['notes']
+# print(type(sum_data))
 
-data['main_accords'] = data['main_accords'].apply(literal_eval)
-data['notes'] = data['notes'].apply(literal_eval)
-
-# list 및 dict value --> 띄어쓰기로 구분된 str으로 변환
-for i in range(len(data['notes'])):
-    if type(data['notes'][i]) == list:
-        data['notes'][i] = ' '.join(data['notes'][i])
-    elif type(data['notes'][i]) == dict:
-        temp = ''
-        for value in data['notes'][i].values():
-            if type(value) == list:
-                temp += ' '.join(value)
-                temp += ' '
-        if temp:
-            data['notes'][i] = temp[:-1]
-
-print('1차')
-# print(data['notes'])
 count_vector = CountVectorizer(ngram_range=(1, 3))
-c_vector_notes = count_vector.fit_transform(data['notes'])
+c_vector_perfume = count_vector.fit_transform(sum_data)
+# print(c_vector_perfume.shape)
 
-print('2차')
+# print('벡터 만드는 시간 : ', time.time() - start)
+
+
 # 코사인 유사도를 구한 벡터 저장
-notes_c_sim = cosine_similarity(c_vector_notes, c_vector_notes).argsort()[:, ::-1]
+perfume_c_sim = cosine_similarity(c_vector_perfume, c_vector_perfume).argsort()[:, ::-1]
+# print(perfume_c_sim.shape)
 
-print('3차')
+print('코사인 유사도 만드는 시간 : ', time.time() - start)
+
 def get_recommend_perfume_list(df, perfume, top=30):
+
     # 특정 perfume (입력값) 데이터 추출
     target_perfume_index = df[df['perfume'] == perfume].index.values
     
     # 코사인 유사도 중 비슷한 코사인 유사도를 가진 정보 추출
-    sim_index = notes_c_sim[target_perfume_index, :top].reshape(-1)
+    sim_index = perfume_c_sim[target_perfume_index, :top].reshape(-1)
+
     # 본인 제외
     sim_index = sim_index[sim_index != target_perfume_index]
 
@@ -52,5 +42,6 @@ def get_recommend_perfume_list(df, perfume, top=30):
     result = df.iloc[sim_index][:10]
     return result
 
-print('4차')
-print(get_recommend_perfume_list(data, perfume='Power by 50 Cent'))
+print(get_recommend_perfume_list(df, perfume='Power by 50 Cent'))
+
+print('종료 시간 : ', time.time() - start)
