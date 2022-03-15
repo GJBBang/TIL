3dac4329eff35bac614476eed275a009c6678999
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-15 16:04:11 +0900

    2022.03.15 데이터 정제 수정

--- python/data_preprocessing/content_based_filtering.py
+++ python/data_preprocessing/content_based_filtering.py
@@ -14,34 +14,34 @@ df = pd.read_json('data_preprocessing.json')
 sum_data = df['main_accords'] + ' ' + df['notes']
 # print(type(sum_data))
 
-count_vector = CountVectorizer(ngram_range=(1, 3))
-c_vector_perfume = count_vector.fit_transform(sum_data)
-# print(c_vector_perfume.shape)
+count_vector = CountVectorizer(ngram_range=(1, 1))
+c_vector_perfume = count_vector.fit_transform(df['main_accords'])
+print(c_vector_perfume.shape)
 
 # print('벡터 만드는 시간 : ', time.time() - start)
 
 
 # 코사인 유사도를 구한 벡터 저장
 perfume_c_sim = cosine_similarity(c_vector_perfume, c_vector_perfume).argsort()[:, ::-1]
-# print(perfume_c_sim.shape)
+print(perfume_c_sim.shape)
 
 print('코사인 유사도 만드는 시간 : ', time.time() - start)
 
-def get_recommend_perfume_list(df, perfume, top=30):
+# def get_recommend_perfume_list(df, perfume, top=30):
 
-    # 특정 perfume (입력값) 데이터 추출
-    target_perfume_index = df[df['perfume'] == perfume].index.values
+#     # 특정 perfume (입력값) 데이터 추출
+#     target_perfume_index = df[df['perfume'] == perfume].index.values
     
-    # 코사인 유사도 중 비슷한 코사인 유사도를 가진 정보 추출
-    sim_index = perfume_c_sim[target_perfume_index, :top].reshape(-1)
+#     # 코사인 유사도 중 비슷한 코사인 유사도를 가진 정보 추출
+#     sim_index = perfume_c_sim[target_perfume_index, :top].reshape(-1)
 
-    # 본인 제외
-    sim_index = sim_index[sim_index != target_perfume_index]
+#     # 본인 제외
+#     sim_index = sim_index[sim_index != target_perfume_index]
 
-    # df 생성
-    result = df.iloc[sim_index].sort_values('total_survey', ascending=False)[:10]
-    return result
+#     # df 생성
+#     result = df.iloc[sim_index].sort_values('total_survey', ascending=False)[:10]
+#     return result
 
-print(get_recommend_perfume_list(df, perfume=df['perfume'][0]))
+# print(get_recommend_perfume_list(df, perfume=df['perfume'][0]))
 
-print('종료 시간 : ', time.time() - start)
+# print('종료 시간 : ', time.time() - start)
--- python/data_preprocessing/data_preprocessing.py
+++ python/data_preprocessing/data_preprocessing.py
@@ -126,24 +126,26 @@ df['id'] = df2['id']
 
 sum_data = df['main_accords'] + ' ' + df['notes']
 
-# count_vector = CountVectorizer(ngram_range=(1, 3))
-# c_vector_perfume = count_vector.fit_transform(sum_data)
+count_vector = CountVectorizer(ngram_range=(1, 3))
+c_vector_perfume = count_vector.fit_transform(sum_data)
 
 # # 코사인 유사도를 구한 벡터 저장
 # perfume_c_sim = cosine_similarity(c_vector_perfume, c_vector_perfume).argsort()[:, ::-1]
+# print(perfume_c_sim)
 # np.save('./saved_c_sim', perfume_c_sim)
 perfume_c_sim = np.load('./saved_c_sim.npy')
+# print(perfume_c_sim)
 
-def get_recommend_perfume_list(df, perfume, top=30):
+def get_recommend_perfume_list(df, idx, top=30):
 
     # 특정 perfume (입력값) 데이터 추출
-    target_perfume_index = df[df['perfume'] == perfume].index.values
+    target_perfume_index = idx
     
     # 코사인 유사도 중 비슷한 코사인 유사도를 가진 정보 추출
     sim_index = perfume_c_sim[target_perfume_index, :top].reshape(-1)
 
     # 본인 제외
-    # sim_index = sim_index[sim_index != target_perfume_index]
+    sim_index = sim_index[sim_index != target_perfume_index]
 
     # df 생성
     result = df.iloc[sim_index].sort_values('total_survey', ascending=False)[:10]
@@ -151,10 +153,9 @@ def get_recommend_perfume_list(df, perfume, top=30):
 
 similar_perfume = []
 for i in range(len(df)):
-    temp = get_recommend_perfume_list(df, df['perfume'][i])
+    temp = get_recommend_perfume_list(df, i)
     similar_perfume.append(list(temp['id']))
 
-print(similar_perfume)
+# print(similar_perfume)
 df2['similar_perfume'] = similar_perfume
-
 df2.to_json('data_preprocessing3.json', orient='records')
