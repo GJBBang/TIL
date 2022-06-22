0cc9e043892eddde2ad583760fefe298017c53af
GJBBang <kzo12341@gmail.com>
2022-06-22 09:34:57 +0900

    fix: 퇴사 python 풀이

--- algorithm/스터디/14501.퇴사.py
+++ algorithm/스터디/14501.퇴사.py
@@ -1,4 +1,3 @@
-import secrets
 import sys
 sys.stdin = open("input.txt")
 
@@ -6,12 +5,13 @@ input = sys.stdin.readline
 N = int(input())
 
 schedule = [(0, 0)]
-dp = [0] * (N + 1)
+dp = [0] * (N + 2)
 for _ in range(N):
     t, p = map(int, input().split())
     schedule.append((t, p))
 
 for i in range(1, N + 1):
-    if i + schedule[i][0] <= N:
-        dp[i + schedule[i][0]] = max(dp[i] + schedule[i][1], dp[i + schedule[i][0] - 1])
-print(dp)
+    for j in range(i + 1, N + 2):
+        if j - i >= schedule[i][0]:
+            dp[j] = max(dp[i] + schedule[i][1], dp[j])
+print(max(dp))
