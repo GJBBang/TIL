5d5ca222bdc7f9cd78bb16ddf31b9ceeb463f6bf
GJBBang <kzo12341@gmail.com>
2022-06-27 15:51:10 +0900

    feat: 맥주마시면서 python 재풀이

--- algorithm/스터디/9205.맥주마시면서.py
+++ algorithm/스터디/9205.맥주마시면서.py
@@ -1,33 +1,57 @@
+'''
+2
+2
+0 0
+1000 0
+1000 1000
+2000 1000
+2
+0 0
+1000 0
+2000 1000
+2000 2000
+'''
+
 import sys
 sys.stdin = open("input.txt")
 
-def find(x, y):
-    global result
-
-    if result == "happy":
-        return
-
-    if abs(arr[-1][0] - x) + abs(arr[-1][1] - y) <= 1000:
-        result = "happy"
-        return
-    
-    for i in range(1, n + 1):
-        if not visited[i] and abs(arr[i][0] - x) + abs(arr[i][1] - y) <= 1000:
-            visited[i] = 1
-            find(arr[i][0], arr[i][1])
-            visited[i] = 0
-
+from collections import deque
 
 input = sys.stdin.readline
-t = int(input())
-for tc in range(t):
+
+def bfs():
+    queue = deque()
+    queue.append(0)
+    visited[0] = True
+    
+    while queue:
+        location = queue.popleft()
+
+        for i in range(1, n + 2):
+            if not visited[i] and songdo[location][i] == 1:
+                queue.append(i)
+                visited[i] = True
+
+
+T = int(input())
+for tc in range(1, T + 1):
     n = int(input())
-    arr = []
+    coordinate = []
     for _ in range(n + 2):
         x, y = map(int, input().split())
-        arr.append((x, y))
+        coordinate.append((x, y))
     
-    result = "sad"
-    visited = [0] * (n + 1)
-    find(arr[0][0], arr[0][1])
-    print(result)
+    songdo = [[0] * (n + 2) for _ in range(n + 2)]
+    visited = [False] * (n + 2)
+
+    for i in range(n + 2):
+        for j in range(i + 1, n + 2):
+            dist = abs(coordinate[i][0] - coordinate[j][0]) + abs(coordinate[i][1] - coordinate[j][1])
+            if dist <= 1000:
+                songdo[i][j] = songdo[j][i] = 1
+    
+    bfs()
+    if visited[-1]:
+        print("happy")
+    else:
+        print("sad")
