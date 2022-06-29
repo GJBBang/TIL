9f4474020c2269c5f2f0686041e7a5d2982deaf1
GJBBang <kzo12341@gmail.com>
2022-06-29 20:39:56 +0900

    feat: 숫자고르기 python 풀이

--- algorithm/스터디/2668.숫자고르기.py
+++ algorithm/스터디/2668.숫자고르기.py
@@ -10,9 +10,19 @@
 '''
 
 import sys
-
-from matplotlib.pyplot import flag
 sys.stdin = open("input.txt")
+
+def dfs(x):
+    if num == arr[x - 1]:
+        result.append(num)
+    else:
+        if not visited[arr[x - 1]]:
+            visited[arr[x - 1]] = 1
+            dfs(arr[x - 1])
+            visited[arr[x - 1]] = 0
+        else:
+            return
+
 
 input = sys.stdin.readline
 
@@ -20,3 +30,16 @@ N = int(input())
 arr = []
 for _ in range(N):
     arr.append(int(input()))
+
+new_arr = list(set(arr))
+result = []
+visited = [0] * (N + 1)
+
+for i in new_arr:
+    num = i
+    visited[i] = 1
+    dfs(i)
+    visited[i] = 0
+
+print(len(result))
+print(*result, sep="\n")
