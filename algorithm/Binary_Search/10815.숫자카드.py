53a03ccbd92a886a3b1b57e3b072f961e5bcb970
GJBBang <kzo12341@gmail.com>
2022-07-01 23:24:47 +0900

    feat: 숫자카드 python 재풀이

--- algorithm/Binary_Search/10815.숫자카드.py
+++ algorithm/Binary_Search/10815.숫자카드.py
@@ -15,23 +15,34 @@ arr.sort()
 M = int(input())
 numbers = list(map(int, input().split()))
 
-def binary_search(num):
-    start = 0
-    end = N - 1
+# def binary_search(num):
+#     start = 0
+#     end = N - 1
 
-    while start <= end:
-        mid = (start + end) // 2
+#     while start <= end:
+#         mid = (start + end) // 2
 
-        if arr[mid] == num:
-            return 1
+#         if arr[mid] == num:
+#             return 1
 
-        if arr[mid] > num:
-            end = mid - 1
-        else:
-            start = mid + 1
+#         if arr[mid] > num:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
 
-    return 0
+#     return 0
+
+# for num in numbers:
+#     print(binary_search(num), end=" ")
+
+# bisect 라이브러리 사용 
+from bisect import bisect_left
 
 for num in numbers:
-    print(binary_search(num), end=" ")
-        
+    index = bisect_left(arr, num)
+    if index + 1 > N:
+        print(0, end=" ")
+    elif arr[index] == num:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
