bab5a6e9c183dfe7be79ddbab97bd6373f938a56
GJBBang <kzo12341@gmail.com>
2022-07-01 23:12:36 +0900

    feat: 숫자카드 python 풀이

---
+++ algorithm/Binary_Search/10815.숫자카드.py
@@ -0,0 +1,37 @@
+'''
+5
+6 3 2 10 -10
+8
+10 9 -5 2 3 4 5 -10
+'''
+
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+M = int(input())
+numbers = list(map(int, input().split()))
+
+def binary_search(num):
+    start = 0
+    end = N - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] == num:
+            return 1
+
+        if arr[mid] > num:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return 0
+
+for num in numbers:
+    print(binary_search(num), end=" ")
+        
