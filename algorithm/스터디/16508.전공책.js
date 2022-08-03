fad639e4c1b4bb0f76d39aef72d52cb7573598be
GJBBang <kzo12341@gmail.com>
2022-08-03 15:41:26 +0900

    feat: 전공책 nodeJS 풀이 임시저장

--- algorithm/스터디/16508.전공책.js
+++ algorithm/스터디/16508.전공책.js
@@ -26,8 +26,17 @@ bookList = [[35000, COMPUTERARCHITECTURE], [47000, ALGORITHM], [43000, NETWORK],
 */
 
 function solution(T, N, bookList) {
+  const visitedBook = new Array(N).fill(0);
+  const visitedAlpha = [];
+  for (i = 0; i < N; i++) {
+    const temp = new Array(bookList[i][1].length).fill(0);
+    visitedAlpha.push(temp);
+  }
+
   let result = 987654321;
   const findMinCost = (k, sumCost) => {
+    if (result <= sumCost) return;
+
     if (k === T.length) {
       if (result > sumCost) {
         result = sumCost;
@@ -36,14 +45,23 @@ function solution(T, N, bookList) {
     }
 
     for (i = 0; i < N; i++) {
-      if (bookList[i][1].includes(T[k])) {
-        findMinCost(k + 1, sumCost + bookList[i][0])
+      for (j = 0; j < bookList[i][1].length; j++) {
+        if (T[k] === bookList[i][1][j] && !visitedAlpha[i][j]) {
+          visitedAlpha[i][j] = 1;
+          if (visitedBook[i]) {
+            findMinCost(k + 1, sumCost);
+          } else {
+            visitedBook[i] = 1;
+            findMinCost(k + 1, sumCost + bookList[i][0]);
+            visitedBook[i] = 0;
+          }
+          visitedAlpha[i][j] = 0;
+        }
       }
     }
   };
 
   findMinCost(0, 0);
-  
 }
 
 solution(inputT, inputN, inputBookList);
