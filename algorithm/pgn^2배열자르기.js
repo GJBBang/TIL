c536b94eaa7958b252665d34f85363ee3b418be1
GJBBang <kzo12341@gmail.com>
2022-11-02 16:28:59 +0900

    feat: n^2배열자르기 nodeJS 풀이

---
+++ algorithm/pgn^2배열자르기.js
@@ -0,0 +1,9 @@
+function solution(n, left, right) {
+  var answer = [];
+  
+  while (left <= right) {
+      answer.push(Math.max(parseInt(left / n), left++ % n) + 1);
+  };
+  
+  return answer;
+}
